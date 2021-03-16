49e6a76a16d959694ba6198502a9876c7a3db730
ttodinator <ttodinator@gmail.com>
2021-03-16 03:22:25 +0100

    Dodata je klasa Magacin koja implementira klasu MagacinInterface

---
+++ magacin/src/magacin/Magacin.java
@@ -0,0 +1,25 @@
+package magacin;
+
+import magacin.interfejs.MagacinInterface;
+
+public class Magacin implements MagacinInterface{
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String vratiInformacije(int sifraArtikla) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
